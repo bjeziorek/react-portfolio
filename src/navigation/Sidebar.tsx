c7fdfce9015132c44f121831dcc6721d2374dc1d
Barbara Jeziorek <barbarajeziorek@gmail.com>
2026-04-14 21:29:27 +0200

    deploy cleaning

--- src/navigation/Sidebar.tsx
+++ src/navigation/Sidebar.tsx
@@ -1,6 +1,6 @@
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Brain, FlaskConical, Home, Puzzle, HeartPlus, BookSearch, LibraryBig, Book } from "lucide-react";
+import { Brain, Home, Book } from "lucide-react";
 import { useTranslation } from "react-i18next";
 import { Box, Flex, Heading } from "@radix-ui/themes";
 import { SidebarNavItem } from "./SidebarNavItem";
